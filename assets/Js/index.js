cb0313675cc503cadd000c7d3fdf999603c466c6
Paola Covone <paoandrec21@gmail.com>
2023-12-27 20:54:57 -0500

    Se realiza el responsive de la página, pronto más cambios.

---
+++ assets/Js/index.js
@@ -0,0 +1,29 @@
+// const burger = document.querySelector(".burger");
+// const navLinks = document.querySelector(".nav-links");
+
+// burger.addEventListener("click", () => {
+//   navLinks.classList.toggle("active");
+// });
+
+// /*Loading*/
+// // Este código se encargará de ocultar el loader, el fondo y el texto cuando la página se cargue completamente
+// window.addEventListener("load", function () {
+//   // Selecciona el div del loader
+//   var loaderWrapper = document.querySelector(".loader-wrapper");
+
+//   // Selecciona el div del texto de bienvenida
+//   var welcomeText = document.querySelector(".welcome-text");
+
+//   // Cambia el tiempo de espera a, por ejemplo, 3000 milisegundos (3 segundos)
+//   setTimeout(function () {
+//     // Oculta el loader, el fondo y el texto de bienvenida
+//     loaderWrapper.style.opacity = "0";
+//     welcomeText.style.opacity = "0";
+
+//     setTimeout(function () {
+//       // Elimina el div del loader, el fondo y el texto de bienvenida
+//       loaderWrapper.style.display = "none";
+//       welcomeText.style.display = "none";
+//     }, 1000); // Puedes ajustar el tiempo de animación aquí
+//   }, 2000); // Cambia este valor al tiempo que desees en milisegundos
+// });
